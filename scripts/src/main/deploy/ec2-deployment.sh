56902b3599ab6f9d267082eea9eb61469b38b582
abninder <abninder@btinternet.com>
2020-05-06 22:46:40 +0100

    checkin

--- scripts/src/main/deploy/ec2-deployment.sh
+++ scripts/src/main/deploy/ec2-deployment.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-DOCKER_SCRIPTS_SRC_PATH="${WORKSPACE}/target/scripts/ec2/*.*"
+DOCKER_SCRIPTS_SRC_PATH="${WORKSPACE}/target/scripts/ec2/"
 SCRIPTS_DESTINATION_FOLDER="/home/ec2-user/scripts"
-
-DOCKER_CONTAINER_STARTUP_CMD="sudo docker-compose up"
 
 ssh-keyscan -H "${EC2_PUBLIC_DNS}" >> ~/.ssh/known_hosts
 
@@ -18,6 +16,6 @@ scp -i "${EC2_PEM_KEY_PATH}" "${DOCKER_SCRIPTS_SRC_PATH}" "ec2-user@${EC2_PUBLIC
 ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" ${SCRIPTS_DESTINATION_FOLDER}/remove-previous-images.sh
 
 #start docker compose
-ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" ${SCRIPTS_DESTINATION_FOLDER}/"${DOCKER_CONTAINER_STARTUP_CMD}"
+ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" ${SCRIPTS_DESTINATION_FOLDER}/start-all-containers.sh
 
 exit $?
