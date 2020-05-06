d40387eaa390205163ffe5ff21db0c7de1ea921f
abninder <abninder@btinternet.com>
2020-05-07 00:14:08 +0100

    checkin

--- scripts/src/main/deploy/ec2-deployment.sh
+++ scripts/src/main/deploy/ec2-deployment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DOCKER_SCRIPTS_SRC_PATH="${WORKSPACE}/target/scripts/ec2"
-SCRIPTS_DESTINATION_FOLDER="/home/ec2-user/scripts/ec2"
+SCRIPTS_DESTINATION_FOLDER="/home/ec2-user/scripts/"
 
 ssh-keyscan -H "${EC2_PUBLIC_DNS}" >> ~/.ssh/known_hosts
 
-echo creating directory ${SCRIPTS_DESTINATION_FOLDER} on "${EC2_PUBLIC_DNS}" if it doesnt exist
-ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" mkdir -p ${SCRIPTS_DESTINATION_FOLDER}
+#echo creating directory ${SCRIPTS_DESTINATION_FOLDER} on "${EC2_PUBLIC_DNS}" if it doesnt exist
+#ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" mkdir -p ${SCRIPTS_DESTINATION_FOLDER}
 
 #copy scripts to ec2 instance
 echo copying scripts from "${DOCKER_SCRIPTS_SRC_PATH}" to ${SCRIPTS_DESTINATION_FOLDER} on "${EC2_PUBLIC_DNS}"
@@ -16,8 +16,6 @@ scp -i "${EC2_PEM_KEY_PATH}" -r "${DOCKER_SCRIPTS_SRC_PATH}" "ec2-user@${EC2_PUB
 ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" ${SCRIPTS_DESTINATION_FOLDER}/remove-previous-images.sh
 
 #start docker compose
-
-
 ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" ${SCRIPTS_DESTINATION_FOLDER}/start-all-containers.sh
 
 exit $?
