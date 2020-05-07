99b74e00fd09785bc0635e5f77067981203a98a7
abninder <abninder@btinternet.com>
2020-05-07 19:40:32 +0100

    checkin

--- scripts/src/main/deploy/ec2-deployment.sh
+++ scripts/src/main/deploy/ec2-deployment.sh
@@ -6,11 +6,12 @@ SCRIPTS_DESTINATION_FOLDER="/home/ec2-user/scripts/ec2"
 ssh-keyscan -H "${EC2_PUBLIC_DNS}" >> ~/.ssh/known_hosts
 
 #echo creating directory ${SCRIPTS_DESTINATION_FOLDER} on "${EC2_PUBLIC_DNS}" if it doesnt exist
-#ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" mkdir -p ${SCRIPTS_DESTINATION_FOLDER}
+ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" mkdir -p ${SCRIPTS_DESTINATION_FOLDER}
 
 #copy scripts to ec2 instance
 echo copying scripts from "${DOCKER_SCRIPTS_SRC_PATH}" to ${SCRIPTS_DESTINATION_FOLDER} on "${EC2_PUBLIC_DNS}"
-scp -i "${EC2_PEM_KEY_PATH}" -r "${DOCKER_SCRIPTS_SRC_PATH}" "ec2-user@${EC2_PUBLIC_DNS}":${SCRIPTS_DESTINATION_FOLDER}
+scp -i "${EC2_PEM_KEY_PATH}" -r "${DOCKER_SCRIPTS_SRC_PATH}/*.sh" "ec2-user@${EC2_PUBLIC_DNS}":${SCRIPTS_DESTINATION_FOLDER}
+scp -i "${EC2_PEM_KEY_PATH}" -r "${DOCKER_SCRIPTS_SRC_PATH}/*.yml" "ec2-user@${EC2_PUBLIC_DNS}":${SCRIPTS_DESTINATION_FOLDER}
 
 #stop the running containers and images if any
 ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" ${SCRIPTS_DESTINATION_FOLDER}/remove-previous-images.sh
