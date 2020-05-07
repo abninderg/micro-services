e05798d367b2cbc34d39fdfe97adda74606bb2f0
abninder <abninder@btinternet.com>
2020-05-07 19:11:13 +0100

    checkin

--- scripts/src/main/deploy/ec2-deployment.sh
+++ scripts/src/main/deploy/ec2-deployment.sh
@@ -6,7 +6,7 @@ SCRIPTS_DESTINATION_FOLDER="/home/ec2-user/scripts/ec2"
 ssh-keyscan -H "${EC2_PUBLIC_DNS}" >> ~/.ssh/known_hosts
 
 #echo creating directory ${SCRIPTS_DESTINATION_FOLDER} on "${EC2_PUBLIC_DNS}" if it doesnt exist
-#ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" mkdir -p ${SCRIPTS_DESTINATION_FOLDER}
+ssh -i "${EC2_PEM_KEY_PATH}" "ec2-user@${EC2_PUBLIC_DNS}" mkdir -p ${SCRIPTS_DESTINATION_FOLDER}
 
 #copy scripts to ec2 instance
 echo copying scripts from "${DOCKER_SCRIPTS_SRC_PATH}" to ${SCRIPTS_DESTINATION_FOLDER} on "${EC2_PUBLIC_DNS}"
