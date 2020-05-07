eef52f725f1b07f048a48240a2ed9bb707db0afe
abninder <abninder@btinternet.com>
2020-05-07 23:45:52 +0100

    checkin

--- scripts/src/main/ec2/start-all-containers.sh
+++ scripts/src/main/ec2/start-all-containers.sh
@@ -1,7 +1,7 @@
 DOCKER_COMPOSE_DIR="$1"
 
 sudo docker login --username=abninder --password=liverpool01
-sudo curl -L https://github.com/docker/compose/releases/download/1.26.0-rc4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo chmod 777 /var/run/docker.sock
 
