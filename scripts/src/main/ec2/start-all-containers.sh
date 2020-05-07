ed5db85e29ef82a679d975eb7456ae92a2a414e1
abninder <abninder@btinternet.com>
2020-05-07 21:21:59 +0100

    checkin

--- scripts/src/main/ec2/start-all-containers.sh
+++ scripts/src/main/ec2/start-all-containers.sh
@@ -3,6 +3,7 @@ DOCKER_COMPOSE_DIR="$1"
 sudo docker login --username=abninder --password=liverpool01
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod 777 /var/run/docker.sock
 
 # shellcheck disable=SC2164
 cd "${DOCKER_COMPOSE_DIR}"
