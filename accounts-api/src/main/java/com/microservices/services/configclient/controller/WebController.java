e26d95be3d16e26b18e3b4a6e10123c5a353c5d8
abninder <abninder@btinternet.com>
2020-05-08 20:20:17 +0100

    checkin

--- accounts-api/src/main/java/com/microservices/services/configclient/controller/WebController.java
+++ accounts-api/src/main/java/com/microservices/services/configclient/controller/WebController.java
@@ -2,11 +2,14 @@ package com.microservices.services.configclient.controller;
 
 import com.microservices.services.configclient.model.AccountDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @RestController
 public class WebController {
@@ -29,8 +32,11 @@ public class WebController {
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(60))
+                .setReadTimeout(Duration.ofSeconds(60))
+                .build();
     }
 
 }
