b04fb7ba7c6b7aff13a8b4d014819921eaf88d7c
abninder <abninder@btinternet.com>
2020-05-08 10:26:00 +0100

    checkin

--- discovery/src/main/java/com/microservices/services/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/microservices/services/discovery/DiscoveryApplication.java
@@ -1,12 +1,26 @@
 package com.microservices.services.discovery;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryApplication {
+
+    @Bean
+    @Profile("!default")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        b.setDataCenterInfo(info);
+        return b;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
