ed99e352d3673e07e91218faaa4889020fe699b7
abninder <abninder@btinternet.com>
2020-05-09 21:45:43 +0100

    checkin

--- gateway/src/main/java/com/microservices/services/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/microservices/services/gateway/GatewayApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableRetry
 public class GatewayApplication {
 
     public static void main(String[] args) {
